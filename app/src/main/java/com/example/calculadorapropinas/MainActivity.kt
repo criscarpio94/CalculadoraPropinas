977133540b64d4eaf3136b6af0240cdcc5e101ab
criscarpio94 <carpiocris94@gmail.com>
2026-05-18 15:15:53 -0500

    "Se agrega TexField y botones de porcentaje"

--- app/src/main/java/com/example/calculadorapropinas/MainActivity.kt
+++ app/src/main/java/com/example/calculadorapropinas/MainActivity.kt
@@ -6,12 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,6 +42,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//Pantalla inicial
 @Composable
 fun  PantallaCalculadora () {
     Column(
@@ -45,9 +53,66 @@ fun  PantallaCalculadora () {
 
     ) {
         Text(
-            text = "Calculadora de Propinas",
+            text = "CALCULADORA DE PROPINAS",
             fontSize = 26.sp
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //Declaracion de variables
+        var montoCuenta = remember { mutableStateOf("" ) }
+        var porcentajePropina = remember { mutableStateOf(0) }
+
+        //TextField para recibir el monto de la cuenta
+        TextField(
+            value = montoCuenta.value,
+            onValueChange = {montoCuenta.value = it},
+            label = { Text("Monto de la cuenta") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Escoja que porcentaje de propina aplicar: "
+        )
+
+        //Botones con porcentajes
+        //Boton 10%
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                porcentajePropina.value = 10}
+        ){
+            Text(
+                text = "10%"
+            )
+        }
+
+        //Boton 15%
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                porcentajePropina.value = 15}
+        ){
+            Text(
+                text = "15%"
+            )
+        }
+
+        //Boton 20%
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                porcentajePropina.value = 20}
+        ){
+            Text(
+                text = "20%"
+            )
+        }
+
+
+
+
+
     }
 }
 
